ffc0cc4d26a52b47e0f2cde79ded79b3f007a762
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2026-03-01 16:49:15 -0330

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
@@ -15,14 +16,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <meta property="og:image" content="/opengraph-image.png" />
-      <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:width" content="1280" />
-      <meta property="og:image:height" content="832" />
-      <meta name="twitter:image" content="/twitter-image.png" />
-      <meta name="twitter:image:type" content="image/png" />
-      <meta name="twitter:image:width" content="1280" />
-      <meta name="twitter:image:height" content="832" />
+      <head>
+        <meta property="og:image" content="/opengraph-image.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1280" />
+        <meta property="og:image:height" content="832" />
+        <meta name="twitter:image" content="/twitter-image.png" />
+        <meta name="twitter:image:type" content="image/png" />
+        <meta name="twitter:image:width" content="1280" />
+        <meta name="twitter:image:height" content="832" />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-DEBK9S5P9M"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-DEBK9S5P9M');`}
+        </Script>
+      </head>
       <body>
         {children}
         <Toaster richColors position="top-center" />
